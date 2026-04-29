59651891ca7520c9b378d5ad03471fc49ef604eb
alexandreluiz-eng <alexandre.luiz@unifesp.br>
2026-04-29 13:36:22 -0300

    Create 1013.c

---
+++ 1013.c
@@ -0,0 +1,16 @@
+using System:
+
+cluss Program
+{
+    static void Main(string[] args)
+    {
+    string[] valores = Console.ReadLine().Split( ' ' ):
+    int a = int. parse(valores[0]):
+    int b = int. parse(valores[1]):
+    int c = int. Parse(valores[2]):
+
+int maiorAB = (a + b + Math .Ans (a - b)) / 2
+
+int maior = (maiorAB + c + Math .Abs(maiorAB - c)) / 2
+
+Console.WriteLine($"{Imaior} eh o maior") ;
