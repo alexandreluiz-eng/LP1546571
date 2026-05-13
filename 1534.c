80fd5872e92ce148f3708066cdfc3f94cb2a6ebe
alexandreluiz-eng <alexandre.luiz@unifesp.br>
2026-05-13 09:17:51 -0300

    1534.c

---
+++ 1534.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int N, i, j;
+
+    while (scanf("%d", &N) != EOF) {
+
+        for (i = 0; i < N; i++) {
+
+            for (j = 0; j < N; j++) {
+
+                if (i == j)
+                    printf("1");
+                else if (i + j == N - 1)
+                    printf("2");
+                else
+                    printf("3");
+            }
+
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
