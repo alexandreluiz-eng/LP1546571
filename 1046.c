b4b7aacd5f06984caab44c108c6e07c7fa3bb830
alexandreluiz-eng <alexandre.luiz@unifesp.br>
2026-05-21 14:30:38 -0300

    1046.c

---
+++ 1046.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int inicio, fim, duracao;
+
+    scanf("%d %d", &inicio, &fim);
+
+    if (inicio < fim) {
+        duracao = fim - inicio;
+    } else if (inicio > fim) {
+        duracao = (24 - inicio) + fim;
+    } else {
+        duracao = 24;
+    }
+
+    printf("O JOGO DUROU %d HORA(S)\n", duracao);
+
+    return 0;
+}
